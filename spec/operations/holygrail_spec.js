30b7a1fc34e9b28ddf6d747efa9489c53fa6b489
kris meermans <kris.meermans@gmail.com>
2019-07-05 09:20:06 +0200

    fix operation check

--- spec/operations/holygrail_spec.js
+++ spec/operations/holygrail_spec.js
@@ -5,7 +5,7 @@
  * DS102: Remove unnecessary code created because of implicit returns
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-const operation = require('../../lib/operations/awesome');
+const operation = require('../../lib/operations/holygrail');
 
 describe("/holygrail", function() {
     it("should have the correct name", () => expect(operation.name).toEqual('Holygrail'));
