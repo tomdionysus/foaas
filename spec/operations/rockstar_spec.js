78114d02cbcd774fa3a515147dd9003f3e052425
Jim Cassidy <jimcassidy@mail.com>
2019-11-26 20:54:30 -0800

    Implement the operations: even, legend and rockstar

---
+++ spec/operations/rockstar_spec.js
@@ -0,0 +1,46 @@
+const operation = require('../../lib/operations/rockstar');
+
+describe("/rockstar", function() {
+  it("should have the correct name", () => expect(operation.name).toEqual('Rockstar'));
+
+  it("should have the correct url", () => expect(operation.url).toEqual('/rockstar/:name/:from'));
+
+  it("should have the correct fields", () =>
+    expect(operation.fields).toEqual([
+      { name: 'Name', field: 'name'},
+      { name: 'From', field: 'from'}
+    ])
+  );
+
+  return describe('register', function() {
+    it('should call app.get with correct url', function() {
+      const app =
+        {get: jasmine.createSpy()};
+
+      operation.register(app,null);
+
+      expect(app.get).toHaveBeenCalledWith('/rockstar/:name/:from', jasmine.any(Function));
+    });
+
+    return it('should call output with correct params', function() {
+      let func = null;
+      const app =
+        {get(url, fn) { return func = fn; }};
+      const output = jasmine.createSpy();
+      operation.register(app, output);
+
+      const req = { 
+        params: {
+          name: "TESTNAME",
+          from: "TESTFROM"
+        }
+      };
+
+      const message = `${req.params.name}, you're a fucking Rock Star!`;
+      const subtitle = `- ${req.params.from}`;
+
+      func(req,'RES');
+      return expect(output).toHaveBeenCalledWith(req, 'RES', message, subtitle);
+    });
+  });
+});
