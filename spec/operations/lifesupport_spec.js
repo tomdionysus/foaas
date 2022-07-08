dc374aa1127d258b415b0ff5d9f0ba6cb42e2727
bahree <amitdrop@desigeek.com>
2022-07-08 09:37:39 -0700

    Yes, I was being sarcastic.

---
+++ spec/operations/lifesupport_spec.js
@@ -0,0 +1,44 @@
+const operation = require('../../lib/operations/lifesupport')
+
+describe('/lifesupport', function () {
+  it('should have the correct name', () => expect(operation.name).toEqual('Lifesupport'))
+
+  it('should have the correct url', () => expect(operation.url).toEqual('/lifesupport/:from'))
+
+  it('should have the correct fields', () =>
+    expect(operation.fields).toEqual([
+      { name: 'From', field: 'from' }
+    ])
+  )
+
+  return describe('register', function () {
+    it('should call app.get with correct url', function () {
+      const app =
+        { get: jasmine.createSpy() }
+
+      operation.register(app, null)
+
+      expect(app.get).toHaveBeenCalledWith('/lifesupport/:from', jasmine.any(Function))
+    })
+
+    return it('should call output with correct params', function () {
+      let func = null
+      const app =
+        { get (url, fn) { return func = fn } }
+      const output = jasmine.createSpy()
+      operation.register(app, output)
+
+      const req = {
+        params: {
+          from: 'TESTFROM'
+        }
+      }
+
+      const message = 'Not saying I hate you, but I would unplug your life support to charge my phone.'
+      const subtitle = `- ${req.params.from}`
+
+      func(req, 'RES')
+      return expect(output).toHaveBeenCalledWith(req, 'RES', message, subtitle)
+    })
+  })
+})
